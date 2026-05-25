5ade869ec5fcd87b732e8e8b8535000488ea6b6c
Thomas <mollynumber9@gmail.com>
2026-05-25 13:39:49 +0200

    Aanpassing workflow locatie te publiceren bestanden

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WKVoorspellingApp/', // 👈 Dit zorgt ervoor dat GitHub Pages de bestanden kan vinden
   server: {
     port: 4173,
   },
